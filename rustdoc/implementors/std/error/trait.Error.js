36814b0131919ac6398f22aa8c57d28304ac3a46
sunshowers <rain@sunshowers.io>
2021-03-11 21:49:48 +0000

    Deploying to gh-pages from  @ db8d53d8464d3693185ca06fd7998fd72e6eddc4 🚀

--- rustdoc/implementors/std/error/trait.Error.js
+++ rustdoc/implementors/std/error/trait.Error.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["camino"] = [{"text":"impl Error for FromPathBufError","synthetic":false,"types":[]},{"text":"impl Error for FromPathError","synthetic":false,"types":[]}];
 implementors["proc_macro2"] = [{"text":"impl Error for LexError","synthetic":false,"types":[]}];
 implementors["serde"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
 implementors["syn"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
